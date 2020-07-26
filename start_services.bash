08616c1ad7f02388a7eac4a32a86f1ddb5511140
Emma Williams <emma.w.dev@gmail.com>
2020-07-26 11:07:31 +0430

    start_services modified to send an api request to start video processing

--- start_services.bash
+++ start_services.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 CONFIG="$1"
+bash /repo/sample_startup.bash $CONFIG  &
 python3 run_processor_core.py --config $CONFIG & 
-python3 run_processor_api.py --config $CONFIG 
+python3 run_processor_api.py --config $CONFIG
