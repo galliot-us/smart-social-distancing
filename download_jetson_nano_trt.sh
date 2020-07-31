e0e735bcabe39910d1333e44b9e05b88c6abc3c2
Json <alec.sadler.2012@gmail.com>
2020-07-31 08:21:48 +0430

    Added TRT_ssd_mobilenet_v2_pedestrian_softbio

--- download_jetson_nano_trt.sh
+++ download_jetson_nano_trt.sh
@@ -1,1 +1,3 @@
 wget https://github.com/Tony607/jetson_nano_trt_tf_ssd/raw/master/packages/jetpack4.3/TRT_ssd_mobilenet_v2_coco.bin -O data/jetson/TRT_ssd_mobilenet_v2_coco.bin
+# TODO: download ssd_mobilenet_v2_pedestrian from neuralet models
+wget https://media.githubusercontent.com/media/JsonSadler/neuralet-models/master/jetson-nano/pedestrian_ssd_mobilenet_v2_softbio/TRT_ssd_mobilenet_v2_pedestrian_softbio.bin -O data/jetson/TRT_ssd_mobilenet_v2_pedestrian_softbio.bin
