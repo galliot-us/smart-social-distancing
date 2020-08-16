bb2a42c08c45f4043683c13ddbf0959f1e58e562
mrn-mln <mehraliangit@gmail.com>
2020-08-17 02:00:49 +0430

    added face_mask classifier inference

--- libs/classifiers/x86/face_mask.py
+++ libs/classifiers/x86/face_mask.py
@@ -1,11 +1,45 @@
+import tensorflow as tf
+import numpy as np
+import pathlib
+import time
+from libs.detectors.utils.fps_calculator import convert_infr_time_to_fps
 
 
 def load_model(model_name):
-    pass
+    # base_url = 'Not Available'
+    # model_file = model_name + '.tar.gz'
+    # model_dir = tf.keras.utils.get_file(
+    #     fname=model_name,
+    #     origin=base_url + model_file,
+    #     untar=True)
+
+    # model_dir = pathlib.Path(model_dir) / "saved_model"
+    model_dir = 'data/x86/face_mask'  # TODO: load automatically after providing a proper model
+
+    model = tf.saved_model.load(str(model_dir), None)
+    print('Classifier model signatures: ', list(model.signatures.keys()))
+    model = model.signatures[
+        'predict']  # 'predict' signature was defined during exporting pb model change it if your signiture is someting else
+
+    return model
+
 
 class Classifier:
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self._config = config
+        self.model_name = self.config.get_section_dict('Classifier')['Name']
+        self.classifier_model = load_model(self.model_name)
+        # Frames Per Second
+        self.fps = None
 
-    def inference(self):
-        pass
+    def inference(self, resized_rgb_image):
+        input_image = np.expand_dims(resized_rgb_image, axis=0)
+        input_tensor = tf.convert_to_tensor(input_image, dtype=tf.float32)
+        t_begin = time.perf_counter()
+        output_dict = self.detection_model(input_tensor)
+        inference_time = time.perf_counter() - t_begin  # Seconds
+        # Calculate Frames rate (fps)
+        self.fps = convert_infr_time_to_fps(inference_time)
+
+        result = np.argmax(output_dict['scores'].numpy())  # returns class id
+        return result
