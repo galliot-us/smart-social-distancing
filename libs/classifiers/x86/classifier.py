c0a28e318ef9e3c1776c169f7d8faa0099e47463
mrn-mln <mehraliangit@gmail.com>
2020-08-17 02:00:24 +0430

    added classifier inference module

--- libs/classifiers/x86/classifier.py
+++ libs/classifiers/x86/classifier.py
@@ -1,8 +1,15 @@
-
-
 class Classifier:
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
+        self.name = self.config.get_section_dict('Classifier')['Name']
 
-    def inference(self):
-        pass
+        if self.name == 'face_mask':
+            from libs.classifiers.x86 import face_mask
+            self.net = face_mask.Classifier(self.config)
+        else:
+            raise ValueError('Not supported network named: ', self.name)
+
+    def inference(self, resized_rgb_image):
+        self.fps = self.net.fps
+        output = self.net.inference(resized_rgb_image)
+        return output
