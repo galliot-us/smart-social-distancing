1d1d8e2cd7fb4323502d10e135d80c0634133c7d
mrn-mln <mehraliangit@gmail.com>
2020-08-19 17:57:06 +0430

    added docstinrg

--- libs/classifiers/x86/face_mask.py
+++ libs/classifiers/x86/face_mask.py
@@ -6,7 +6,14 @@ from libs.detectors.utils.fps_calculator import convert_infr_time_to_fps
 
 
 def load_model(model_name):
-    # base_url = 'Not Available'
+    """
+    Args:
+        model_name: Download the model based on its name and load the model
+
+    Returns:
+
+    """
+    base_url = 'Not Available'
     # model_file = model_name + '.tar.gz'
     # model_dir = tf.keras.utils.get_file(
     #     fname=model_name,
@@ -24,6 +31,14 @@ def load_model(model_name):
 
 
 class Classifier:
+    """
+    Perform image classification with the given model. The model is a protobuf
+    file which if the classifier can not find it at the path it will download it
+    from neuralet repository automatically.
+
+    :param config: Is a ConfigEngine instance which provides necessary parameters.
+    """
+
     def __init__(self, config):
         self.config = config
         self.model_name = self.config.get_section_dict('Classifier')['Name']
@@ -31,8 +46,18 @@ class Classifier:
         # Frames Per Second
         self.fps = None
 
-    def inference(self, resized_rgb_image:list):
-        
+    def inference(self, resized_rgb_image: list) -> list:
+        """
+        Inference function sets input tensor to input image and gets the output.
+        The interpreter instance provides corresponding class id output which is used for creating result
+
+        Args:
+            resized_rgb_image: Float numpy array with shape (no_images, img_height, img_width, channels)
+
+        Returns:
+            result: List of class id for each input image [0, 0, 1, 1, 0]
+
+        """
         if resized_rgb_image == []:
             return resized_rgb_image
         # iinput_image = np.expand_dims(resized_rgb_image, axis=0) 
