85b3980939d71d8152e922fdb3a44ecb370fb6cc
mrn-mln <mehraliangit@gmail.com>
2020-08-23 03:50:24 +0430

    added docstring

--- libs/classifiers/edgetpu/face_mask.py
+++ libs/classifiers/edgetpu/face_mask.py
@@ -9,6 +9,13 @@ from libs.detectors.utils.fps_calculator import convert_infr_time_to_fps
 
 
 class Classifier:
+    """
+    Perform image classification with the given model. The model is an int8 quantized tflite
+    file which if the classifier can not find it at the path it will download it
+    from neuralet repository automatically.
+
+    :param config: Is a ConfigEngine instance which provides necessary parameters.
+    """
 
     def __init__(self, config):
         self.config = config
@@ -43,12 +50,19 @@ class Classifier:
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
-        # # Get class id from config
-        # self.class_id = int(self.config.get_section_dict('')['ClassID'])
-        # self.score_threshold = float(self.config.get_section_dict('')['MinScore'])
 
     def inference(self, resized_rgb_image):
-        # Fill input tensor with input_image
+        """
+        Inference function sets input tensor to input image and gets the output.
+        The interpreter instance provides corresponding class id output which is used for creating result
+
+        Args:
+            resized_rgb_image: List of images with shape (no_images, img_height, img_width, channels)
+
+        Returns:
+            result: List of class id for each input image [0, 0, 1, 1, 0]
+
+        """
         if resized_rgb_image == []:
             return resized_rgb_image
         resized_rgb_image = resized_rgb_image * 255.0
