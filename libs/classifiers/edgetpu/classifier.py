bf6e328f988515b00ae4beb87e64bb15dfe3020f
mrn-mln <mehraliangit@gmail.com>
2020-10-11 21:30:35 +0330

    supported face-mask fro edgetpu devices

--- libs/classifiers/edgetpu/classifier.py
+++ libs/classifiers/edgetpu/classifier.py
@@ -1,16 +1,16 @@
 class Classifier:
     """
-    Classifier class is a high level class for classifying images using edgetpu devices.
+    Classifier class is a high-level class for classifying images using EdgeTPU devices.
     When an instance of the Classifier is created you can call inference method and feed your
     input image in order to get the classifier results.
-
-    :param config: Is a ConfigEngine instance which provides necessary parameters.
+    :param config: Is a Config instance which provides necessary parameters.
     """
+
     def __init__(self, config):
         self.config = config
         self.name = self.config.get_section_dict('Classifier')['Name']
 
-        if self.name == 'face_mask':
+        if self.name == 'OFMClassifier':
             from libs.classifiers.edgetpu import face_mask
             self.net = face_mask.Classifier(self.config)
         else:
@@ -18,5 +18,5 @@ class Classifier:
 
     def inference(self, resized_rgb_image):
         self.fps = self.net.fps
-        output = self.net.inference(resized_rgb_image)
-        return output
+        output, scores = self.net.inference(resized_rgb_image)
+        return output, scores
--- libs/classifiers/edgetpu/face_mask.py
+++ libs/classifiers/edgetpu/face_mask.py
@@ -19,29 +19,13 @@ class Classifier:
 
     def __init__(self, config):
         self.config = config
-        # Get the model name from the config
-        self.model_name = self.config.get_section_dict('Classifier')['Name']
-        # Frames Per Second
+        self.model_name = "OFMClassifier_edgetpu.tflite"
+        self.model_path = 'libs/classifiers/edgetpu/' + self.model_name
         self.fps = None
-        self.model_file = 'mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite'  # TODO: remove after testing
-        self.model_path = '/repo/data/edgetpu/' + self.model_file
-
-        # Get the model .tflite file path from the config.
-        # If there is no .tflite file in the path it will be downloaded automatically from base_url
-
-        # -_- -_- TODO: uncomment when a proper face-mask classifier is available -_- -_-
-        # user_model_path = self.config.get_section_dict('Classifier')['ModelPath']
-        # if len(user_model_path) > 0:
-        #     print('using %s as model' % user_model_path)
-        #     self.model_path = user_model_path
-        # else:
-        #     base_url = 'N/A'
-        #     url = base_url + self.model_name + '/' + self.model_file
-        #
-        #     if not os.path.isfile(self.model_path):
-        #         print('model does not exist under: ', self.model_path, 'downloading from ', url)
-        #         wget.download(url, self.model_path)
-        # -_- -_- -_- -_- -_- -_- -_- -_- -_- -_- -_- -_- -_- -_- -_- -_- -_- -_-
+        if not os.path.isfile(self.model_path):
+            url = "https://raw.githubusercontent.com/neuralet/neuralet-models/master/edge-tpu/OFMClassifier/OFMClassifier_edgetpu.tflite"
+            print("model does not exist under: ", self.model_path, "downloading from ", url)
+            wget.download(url, self.model_path)
 
         # Load TFLite model and allocate tensors
         self.interpreter = Interpreter(self.model_path, experimental_delegates=[load_delegate("libedgetpu.so.1")])
@@ -50,35 +34,35 @@ class Classifier:
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
-
-    def inference(self, resized_rgb_image):
+    def inference(self, resized_rgb_image) -> list:
         """
         Inference function sets input tensor to input image and gets the output.
         The interpreter instance provides corresponding class id output which is used for creating result
-
         Args:
-            resized_rgb_image: List of images with shape (no_images, img_height, img_width, channels)
-
+            resized_rgb_image: Array of images with shape (no_images, img_height, img_width, channels)
         Returns:
-            result: List of class id for each input image [0, 0, 1, 1, 0]
-
+            result: List of class id for each input image. ex: [0, 0, 1, 1, 0]
+            scores: The classification confidence for each class. ex: [.99, .75, .80, 1.0]
         """
-        if resized_rgb_image == []:
+        if np.shape(resized_rgb_image)[0] == 0:
             return resized_rgb_image
+        resized_rgb_image = (resized_rgb_image * 255).astype("uint8")
         result = []
-        
-        t_begin = time.perf_counter()
+        net_results = []
         for img in resized_rgb_image:
-            img = img * 255.0
-            img = np.array(img, dtype=np.uint8)
-            input_image = np.expand_dims(img, axis=0)
-            self.interpreter.set_tensor(self.input_details[0]["index"], input_image)
+            img = np.expand_dims(img, axis=0)
+            self.interpreter.set_tensor(self.input_details[0]["index"], img)
+            t_begin = time.perf_counter()
             self.interpreter.invoke()
-            out = self.interpreter.get_tensor(self.output_details[0]["index"])
-            result.append(int(np.argmax(out)))
-            
-        inference_time = time.perf_counter() - t_begin  # Second
-        self.fps = convert_infr_time_to_fps(inference_time)
-        # The function `get_tensor()` returns a copy of the tensor data.
-        # Use `tensor()` in order to get a pointer to the tensor.
-        return result
+            inference_time = time.perf_counter() - t_begin  # Second
+            self.fps = convert_infr_time_to_fps(inference_time)
+            net_output = self.interpreter.get_tensor(self.output_details[0]['index'])[0]
+            net_results.append(net_output)
+            result.append(np.argmax(net_output))  # returns class id
+
+        # TODO: optimized without for
+        scores = []
+        for i, itm in enumerate(net_results):
+            scores.append(1)
+
+        return result, scores
