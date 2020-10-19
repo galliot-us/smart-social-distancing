f5fa6cea0bed5c38da6dd211443da0054bcffe2c
Pablo Grill <pablo.grill@xmartlabs.com>
2020-10-19 09:13:45 -0300

    Changing the source square size taking into accout the destination points.

--- libs/utils/camera_calibration.py
+++ libs/utils/camera_calibration.py
@@ -4,16 +4,15 @@ import os
 
 from pathlib import Path
 from pydantic import conlist, BaseModel
+from scipy.spatial import distance
 
 
 class ConfigHomographyMatrix(BaseModel):
-    pts_source: conlist(conlist(float, min_items=2, max_items=2), min_items=4, max_items=4)
     pts_destination: conlist(conlist(float, min_items=2, max_items=2), min_items=4, max_items=4)
 
     class Config:
         schema_extra = {
             'example': {
-                'pts_source': [[0., 0.], [0., 1.], [1., 1.], [1., 0.]],
                 'pts_destination': [[130., 310.], [45., 420.], [275., 420.], [252., 310.]]
             }
         }
@@ -21,7 +20,17 @@ class ConfigHomographyMatrix(BaseModel):
 
 def compute_and_save_inv_homography_matrix(points: ConfigHomographyMatrix, destination: str):
     Path(os.path.dirname(destination)).mkdir(parents=True, exist_ok=True)
-    h, _ = cv.findHomography(np.float32(points.pts_source), np.float32(points.pts_destination))
+    pts_destination = np.float32(points.pts_destination)
+    distances_bt_points = []
+    for i in range(4):
+        distances_bt_points.append(
+            distance.euclidean(points.pts_destination[i], points.pts_destination[(i+1)%4])
+        )
+    square_size = np.average(distances_bt_points)
+    pts_sources = np.float32(
+        [[0, 0], [0, square_size], [square_size , square_size], [square_size, 0]]
+    )
+    h, _ = cv.findHomography(pts_sources, pts_destination)
     h_inv = np.linalg.inv(h).flatten()
     h_inv = ' '.join(map(str, h_inv))
     with open(destination, 'w') as f:
