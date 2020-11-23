bffc0972da3da661cde8817271e6373fcda3d247
Renzo Gambone <renzodgc12@gmail.com>
2020-11-23 10:29:06 -0300

    changed field from str to bool

--- api/models/config_keys.py
+++ api/models/config_keys.py
@@ -24,7 +24,7 @@ class NotificationConfig(SnakeModel):
     violationThreshold: Optional[int] = Field(0, example=100)
     notifyEveryMinutes: Optional[int] = Field(0, example=15)
     emails: Optional[str] = Field("", example='john@email.com,doe@email.com')
-    enableSlackNotifications: Optional[str] = Field(False, example=False)
+    enableSlackNotifications: Optional[bool] = Field(False, example=False)
     dailyReport: Optional[bool] = Field(False, example=True)
     dailyReportTime: Optional[str] = Field('06:00', example='06:00')
 
