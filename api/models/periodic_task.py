23e7ea12db8bf812474c44eb141a285fb19db327
Pablo Grill <pablo.grill@xmartlabs.com>
2020-12-11 17:21:57 -0300

    Update README and config files.

--- api/models/periodic_task.py
+++ api/models/periodic_task.py
@@ -1,14 +1,15 @@
-from pydantic import validator
+from pydantic import Field, validator
 from typing import List
 
 from .base import OptionalSectionConfig, SnakeModel
 
 
 class PeriodicTaskDTO(OptionalSectionConfig):
+    liveInterval: int = Field(example=10)
 
     @validator("name")
     def validate_name(cls, value):
-        if value != "reports":
+        if value != "metrics":
             raise ValueError(f"Not supported periodic task named: {value}")
         return value
 
