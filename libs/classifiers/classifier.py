6e4d98cdd7043d056eb6808dc6fdf1ca6f288371
Undefined References <undefined.references@gmail.com>
2020-12-16 16:30:53 +0330

    added OFM classifier for jetson

--- libs/classifiers/classifier.py
+++ libs/classifiers/classifier.py
@@ -1,17 +1,24 @@
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
 class Classifier:
 
     def __init__(self, config):
         self.config = config
         self.classifier = None
         classifier_section = self.config.get_section_dict("Classifier")
-        if classifier_section["Device"] == "EdgeTPU":
+        if classifier_section["Device"] == "Jetson":
+            from .jetson.classifier import Classifier as JetClassifier
+            self.classifier = JetClassifier(self.config)
+        elif classifier_section["Device"] == "EdgeTPU":
             from .edgetpu.classifier import Classifier as EdgeClassifier
             self.classifier = EdgeClassifier(self.config)
         elif classifier_section["Device"] in ["x86", "x86-gpu"]:
             from .x86.classifier import Classifier as X86Classifier
             self.classifier = X86Classifier(self.config)
         else:
-            raise ValueError(f"Not supported device named: {classifier_section['Device']}")
+            raise ValueError(f"Classifier: Not supported device named: {classifier_section['Device']}")
         self.min_threshold = float(self.config.get_section_dict("Classifier").get("MinScore", 0.75))
 
     def inference(self, objects):
