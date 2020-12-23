fb884d146955b95837261cf9b0a9f7f84c1dc080
Pablo Grill <pablo.grill@xmartlabs.com>
2020-12-23 09:26:50 -0300

    Change screenshot logger logic.

--- libs/loggers/source_loggers/file_system_logger.py
+++ libs/loggers/source_loggers/file_system_logger.py
@@ -19,18 +19,16 @@ class FileSystemLogger(RawDataLogger):
         self.objects_log_directory = os.path.join(self.log_directory, self.camera_id, "objects_log")
         os.makedirs(self.objects_log_directory, exist_ok=True)
 
-        self.screenshot_period = float(self.config.get_section_dict(logger)["ScreenshotPeriod"]) * 60
-        self.start_time = time.time()
         # config.ini uses minutes as the unit for ScreenshotPeriod
-        self.screenshot_path = os.path.join(self.config.get_section_dict("App")["ScreenshotsDirectory"], self.camera_id)
+        self.screenshot_period = float(self.config.get_section_dict(logger)["ScreenshotPeriod"]) * 60
+        self.last_screeenshot_time = time.time()
+        self.screenshot_path = os.path.join(self.config.get_section_dict(logger)["ScreenshotsDirectory"], self.camera_id)
         if not os.path.exists(self.screenshot_path):
             os.makedirs(self.screenshot_path)
 
     def save_screenshot(self, cv_image):
-        dir_path = f'{self.screenshot_path}/default.jpg'
-        if not os.path.exists(dir_path):
-            logger.info(f"Saving default screenshot for {self.camera_id}")
-            cv.imwrite(f'{self.screenshot_path}/default.jpg', cv_image)
+        logger.info(f"Saving default screenshot for {self.camera_id}")
+        cv.imwrite(f'{self.screenshot_path}/default.jpg', cv_image)
 
     def log_objects(self, objects, violating_objects, violating_objects_index_list, violating_objects_count,
                     detected_objects_cout, environment_score, time_stamp, version):
@@ -49,11 +47,8 @@ class FileSystemLogger(RawDataLogger):
                  "Detections": str(objects), "ViolationsIndexes": str(violating_objects_index_list)})
 
     def update(self, cv_image, objects, post_processing_data, fps):
-        violating_objects = post_processing_data.get("violating_objects", [])
-        # Save a screenshot only if the period is greater than 0, a violation is detected, and the minimum period
-        # has occured
-        if (self.screenshot_period > 0) and (time.time() > self.start_time + self.screenshot_period) and (
-                len(violating_objects) > 0):
-            self.start_time = time.time()
+        # Save a screenshot only if the period is greater than 0, and the minimum period has occured
+        if (self.screenshot_period > 0) and (time.time() > self.last_screeenshot_time + self.screenshot_period):
+            self.last_screeenshot_time = time.time()
             self.save_screenshot(cv_image)
         super().update(cv_image, objects, post_processing_data, fps)
