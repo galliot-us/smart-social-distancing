93f60128895f207b200c17270a69c173c4c77d09
Undefined References <undefined.references@gmail.com>
2020-12-30 12:25:18 +0330

    model download urls updated, tensorrt precision added to classifier

--- generate_tensorrt.bash
+++ generate_tensorrt.bash
@@ -6,36 +6,40 @@ imageSize=$(sed -nr "/^\[Detector\]/ { :l /^ImageSize[ ]*=/ { s/.*=[ ]*//; p; q;
 IFS=',' read -a marray <<< "$imageSize"
 width=${marray[0]}
 height=${marray[1]}
+
 mkdir -p /repo/data/onnx
 onnx_name_openpifpaf="/repo/data/onnx/openpifpaf_resnet50_${width}_${height}.onnx"
 onnx_name_face_mask="/repo/data/onnx/ofm_face_mask.onnx"
+
+onnx_openpifpaf_download_url="https://media.githubusercontent.com/media/neuralet/neuralet-models/master/ONNX/openpifpaf_12a4/openpifpaf_resnet50_${width}_${height}.onnx"
 
 if [[ ! $run_on_jetson ]] && [[ ! -f $onnx_name_openpifpaf ]]; then
     echo "############## exporting ONNX from OpenPifPaf ##################"
     python3 -m openpifpaf.export_onnx --outfile $onnx_name_openpifpaf  --checkpoint resnet50  --input-width $width --input-height $height
    
 elif [[ $run_on_jetson ]] && [[ ! -f $onnx_name_openpifpaf  ]]; then
-    wget https://media.githubusercontent.com/media/neuralet/neuralet-models/c874b2bcee0521d770d3480ed5fef25643160abd/ONNX/openpifpaf_12a4/openpifpaf_resnet50_321_193.onnx -O $onnx_name_openpifpaf
+    wget $onnx_openpifpaf_download_url -O $onnx_name_openpifpaf
 fi
 
 if [ ! -f $onnx_name_face_mask ]; then
-    wget https://media.githubusercontent.com/media/neuralet/neuralet-models/c874b2bcee0521d770d3480ed5fef25643160abd/ONNX/OFMClassifier/OFMClassifier.onnx -O $onnx_name_face_mask
+    wget https://media.githubusercontent.com/media/neuralet/neuralet-models/master/ONNX/OFMClassifier/OFMClassifier.onnx -O $onnx_name_face_mask
 fi
 
 mkdir -p /repo/data/tensorrt
 
-precision=$(sed -nr "/^\[Detector\]/ { :l /^TensorrtPrecision[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $config)
-tensorrt_name_openpifpaf="/repo/data/tensorrt/openpifpaf_resnet50_${width}_${height}_d${precision}.trt"
+precision_detector=$(sed -nr "/^\[Detector\]/ { :l /^TensorrtPrecision[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $config)
+tensorrt_name_openpifpaf="/repo/data/tensorrt/openpifpaf_resnet50_${width}_${height}_d${precision_detector}.trt"
 
 if [ ! -f $tensorrt_name_openpifpaf ]; then
     echo "############## Generating TensorRT Engine for openpifpaf ######################"
-    onnx2trt $onnx_name_openpifpaf -o $tensorrt_name_openpifpaf -d $precision -b 1
+    onnx2trt $onnx_name_openpifpaf -o $tensorrt_name_openpifpaf -d $precision_detector -b 1
 fi
 
-tensorrt_name_face_mask="/repo/data/tensorrt/ofm_face_mask_d${precision}.trt"
-
+precision_classifier=$(sed -nr "/^\[Classifier\]/ { :l /^TensorrtPrecision[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $config)
+tensorrt_name_face_mask="/repo/data/tensorrt/ofm_face_mask_d${precision_classifier}.trt"
+ls $onnx_name_face_mask
 if [ ! -f $tensorrt_name_face_mask ]; then
     echo "############## Generating TensorRT Engine for face_mask classifier ##############"
-    onnx2trt $onnx_name_face_mask -o $tensorrt_name_face_mask -d $precision -b 1
+    onnx2trt $onnx_name_face_mask -o $tensorrt_name_face_mask -d $precision_classifier -b 1
 fi
 
