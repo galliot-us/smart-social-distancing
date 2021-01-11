1bd65d92cdd57863202c4d3e29347d3bf3f0f774
Pablo Grill <pablo.grill@xmartlabs.com>
2021-01-11 17:50:39 -0300

    Include performance logging in the processor.[C

--- libs/cv_engine.py
+++ libs/cv_engine.py
@@ -2,13 +2,20 @@ import cv2 as cv
 import logging
 import numpy as np
 import os
+
+from datetime import datetime
+from statistics import mean
+
 from libs.classifiers.classifier import Classifier
 from libs.trackers.tracker import Tracker
 from libs.loggers.source_loggers.logger import Logger
 from libs.detectors.detector import Detector
 from libs.source_post_processors.source_post_processor import SourcePostProcessor
 
+
 logger = logging.getLogger(__name__)
+FRAMES_LOG_BATCH_SIZE = 100
+LOG_SECTIONS = ["Detector", "Classifier", "Tracker", "Post Processors"]
 
 
 class CvEngine:
@@ -40,6 +47,13 @@ class CvEngine:
                 self.loggers.append(Logger(self.config, source, l_name))
         self.running_video = False
 
+        self.log_performance = self.config.get_boolean("App", "LogPerformance")
+        if self.log_performance:
+            self.last_log_time = None
+            self.log_detail = {}
+            for section in LOG_SECTIONS:
+                self.log_detail[section] = []
+
     def __process(self, cv_image):
         """
         return object_list list of  dict for each obj,
@@ -50,29 +64,50 @@ class CvEngine:
         cv_image = cv.resize(cv_image, self.resolution)
 
         # Execute detector
+        begin_time = datetime.now()
         tmp_objects_list, detection_scores, class_ids, detection_bboxes, classifier_objects = self.detector.inference(cv_image)
+        detector_time = (datetime.now() - begin_time).total_seconds()
 
         # Execute classifier and tracker
         if self.classifier:
+            begin_time = datetime.now()
             classifier_results, classifier_scores = self.classifier.inference(classifier_objects)
+            classifier_time = (datetime.now() - begin_time).total_seconds()
+
+        begin_time = datetime.now()
         tracks = self.tracker.update(detection_bboxes, class_ids, detection_scores)
+        tracker_time = (datetime.now() - begin_time).total_seconds()
+
         idx = 0
         for obj in tmp_objects_list:
+            begin_time = datetime.now()
             self.tracker.object_post_process(obj, tracks)
+            tracker_time += (datetime.now() - begin_time).total_seconds()
+
             if self.classifier is not None:
+                begin_time = datetime.now()
                 if obj.get("face") is not None:
                     self.classifier.object_post_process(obj, classifier_results[idx], classifier_scores[idx])
                     idx = idx + 1
                 else:
                     self.classifier.object_post_process(obj, None, None)
+                classifier_time += (datetime.now() - begin_time).total_seconds()
 
         # Execute post processors
         post_processing_data = {
             "tracks": tracks
         }
+        begin_time = datetime.now()
         for post_processor in self.post_processors:
             cv_image, tmp_objects_list, post_processing_data = post_processor.process(
                 cv_image, tmp_objects_list, post_processing_data)
+        post_processors_time = (datetime.now() - begin_time).total_seconds()
+        if self.log_performance:
+            self.log_detail["Detector"].append(detector_time)
+            if self.classifier:
+                self.log_detail["Classifier"].append(classifier_time)
+            self.log_detail["Tracker"].append(tracker_time)
+            self.log_detail["Post Processors"].append(post_processors_time)
         return cv_image, tmp_objects_list, post_processing_data
 
     def process_video(self, video_uri):
@@ -95,10 +130,20 @@ class CvEngine:
         while input_cap.isOpened() and self.running_video:
             _, cv_image = input_cap.read()
             if np.shape(cv_image) != ():
+
                 cv_image, objects, post_processing_data = self.__process(cv_image)
                 frame_num += 1
-                if frame_num % 100 == 1:
+                if frame_num % FRAMES_LOG_BATCH_SIZE == 1:
                     logger.info(f'processed frame {frame_num} for {video_uri}')
+                    if self.log_performance:
+                        if self.last_log_time:
+                            logger.info(f"FPS: {FRAMES_LOG_BATCH_SIZE / (datetime.now() - self.last_log_time).total_seconds()}")
+                            for section in LOG_SECTIONS:
+                                logger.info(f"Average {section} time: {mean(self.log_detail[section])}")
+                    self.last_log_time = datetime.now()
+                    for section in LOG_SECTIONS:
+                        self.log_detail[section] = []
+
                 for source_logger in self.loggers:
                     source_logger.update(cv_image, objects, post_processing_data, self.detector.fps)
         input_cap.release()
