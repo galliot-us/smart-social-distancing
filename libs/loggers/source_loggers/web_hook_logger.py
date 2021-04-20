0a6c39b9d531054e259fe39a0d3682611a9f3923
Pablo Grill <pablo.grill@xmartlabs.com>
2021-04-20 16:31:37 -0300

    Improve webhook logger.

--- libs/loggers/source_loggers/web_hook_logger.py
+++ libs/loggers/source_loggers/web_hook_logger.py
@@ -17,24 +17,35 @@ class WebHookLogger(RawDataLogger):
         self.web_hook_authorization = config.get_section_dict(logger)["Authorization"]
         self.pending_requests = []
 
+    def _process_object(self, object):
+        return {
+            "position": [float(p) for p in object["position"]],
+            "bbox_real": [float(br) for br in object["bbox_real"]],
+            "bbox": [float(bb) for bb in object["bbox"]],
+            "tracking_id": int(object["tracking_id"])
+        }
+
     def log_objects(self, objects, violating_objects, violating_objects_index_list, violating_objects_count,
                     detected_objects_cout, environment_score, time_stamp, version):
         request_data = {
-            "camera_id": self.camera_id,
             "version": version,
             "timestamp": time_stamp,
             "detected_objects": detected_objects_cout,
             "violating_objects": violating_objects_count,
             "environment_score": environment_score,
-            "detections": str(objects),
-            "violations_indexes": str(violating_objects_index_list)
+            "detections": [self._process_object(o) for o in objects],
+            "violations_indexes": [int(v) for v in violating_objects_index_list]
         }
         headers = {"content-type": "application/json"}
         if self.web_hook_authorization:
             headers["Authorization"] = self.web_hook_authorization
         self.pending_requests.append(request_data)
         try:
-            response = requests.put(self.web_hook_endpoint, json=self.pending_requests, headers=headers)
+            request_data = {
+                "camera_id": self.camera_id,
+                "raw_data": self.pending_requests
+            }
+            response = requests.put(self.web_hook_endpoint, json=request_data, headers=headers)
         except ConnectionError:
             logger.error(f"Connection with endpoint {self.web_hook_endpoint} can't be established")
         except Exception as e:
