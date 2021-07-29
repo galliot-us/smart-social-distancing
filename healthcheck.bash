061c3dc8c1428f13a8cc70d0663f7057b0af467b
Pablo Grill <pablo.grill@xmartlabs.com>
2021-07-29 14:57:19 -0300

    Restart api service when in's stucked

--- healthcheck.bash
+++ healthcheck.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
+if ! curl --fail -o /dev/null -m 5 0.0.0.0:8000/docs; then
+    # The api service is not working or is giving timeout.
+    # Restart it
+    eval "supervisorctl -c supervisord.conf restart api"
+fi;
 if fatalError=$(supervisorctl -c supervisord.conf status all | grep -i "FATAL\|UNKNOWN"); then
     exit 1;
 else
