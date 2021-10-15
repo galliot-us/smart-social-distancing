7853bae534a0e05bfb84f08175a51748a54aa3ec
Renzo Gambone <renzodgc12@gmail.com>
2021-10-15 15:59:04 -0300

    fix path

--- exporters/jetson/generate_mobilenet_tensorrt.bash
+++ exporters/jetson/generate_mobilenet_tensorrt.bash
@@ -14,7 +14,7 @@ else
 		wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_coco_2018_03_29.tar.gz -P /repo$relative_path/data/jetson/
 	fi
 	tar -xvf /repo$relative_path/data/jetson/ssd_mobilenet_v2_coco_2018_03_29.tar.gz --no-same-owner -C /repo$relative_path/data/jetson/
-	pb_file="$relative_path/data/jetson/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb"
+	pb_file="/repo$relative_path/data/jetson/ssd_mobilenet_v2_coco_2018_03_29/frozen_inference_graph.pb"
 	adaptive_model=0
 	num_classes=1
 
@@ -22,4 +22,4 @@ fi
 
 
 echo "************  Generating TensorRT from: $pb_file  **************"
-python3 $relative_path/exporters/jetson/trt_exporter.py --pb_file $pb_file --out_dir $relative_path/data/jetson --neuralet_adaptive_model $adaptive_model --num_classes $num_classes
+python3 /repo$relative_path/exporters/jetson/trt_exporter.py --pb_file $pb_file --out_dir /repo$relative_path/data/jetson --neuralet_adaptive_model $adaptive_model --num_classes $num_classes
